f1ac8f7509bdc5db3eb4e6212dc4926a20f5e4ba
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-13 16:17:05 -0300

    exercicio basico 1

---
+++ python_exercicios/exercicio_1.py
@@ -0,0 +1,26 @@
+""" 
+Exercício de fixação 1: Elabore um algoritmo que peça ao usuário um número 
+e informe o quadrado dele (isto é, o número multiplicado por ele mesmo).
+"""
+
+numero = int( input("Digite um número:"))
+
+resultado = numero * 2
+
+print(f"O resultado foi de: {resultado} " )
+
+
+""" 
+PSEUDOCÓDIGO
+
+Entrada:
+
+Saída: 
+
+Passos:
+- 
+- 
+-
+-
+- 
+"""
