ccc34bfac366c1994de14891d8d914a9cc14f7f8
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-14 11:27:45 -0300

    atualizado  calculadora simples

--- python_exercicios/calculadora_simples.py
+++ python_exercicios/calculadora_simples.py
@@ -3,19 +3,13 @@ Calculadora simples
 """
 
 primeiro_valor = float(input("Informe o primeiro valor: "))
-
 segundo_valor = float(input("Informe o segundo valor: "))
 
  
-
 soma = primeiro_valor + segundo_valor
-
 subtracao = primeiro_valor - segundo_valor
-
 multiplicacao = primeiro_valor * segundo_valor
-
 divisao = primeiro_valor / segundo_valor
-
 resto = primeiro_valor % segundo_valor
 
 print(f"Soma:{soma:.0f}")
@@ -23,3 +17,10 @@ print(f"Subtração: {subtracao:.2f}")
 print(f"Multiplicação: {multiplicacao:.2f}")
 print(f"Divisao: {divisao:.2f}")
 print(f"Resto: {resto}")
+
+# Ou apenas adicionar o nome dads variaveis a função print
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(resto)
