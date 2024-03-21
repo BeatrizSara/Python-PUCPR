c7317b8bd42e43c957fafbe502e9f84858140c2e
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-20 21:44:08 -0300

    exercicio 2

---
+++ python_exercicios/introducao_python/exercicio_2.py
@@ -0,0 +1,25 @@
+""" 
+Exercício de fixação 2: Elabore um algoritmo que solicite dois números ao usuário e exiba a soma deles.
+"""
+
+numero_um = int(input("Informe um número: "))
+numero_dois = int(input("Informe um segundo numero: "))
+
+soma = numero_um + numero_dois
+
+print(f"A soma de {numero_um} + {numero_dois} é {soma}")
+
+""" 
+Entrada: Dois numeros inteiros
+
+Saída: A soma de dois numeros inteiros
+
+Passos:
+    a. Peça ao usuario que informe dois numeros inteiros
+    b. Guarde o que o usuario digitou na variavel "numero_um" 
+     - peça ao usuario para fornecer um numero
+    c. Guarde o que o usuario digitou na variavel "numero_dois"
+     - peça ao usuario para fornecer um numero
+    d. Soma = numero_um + numero_dois
+    e. Exiba a variavel "soma" para o usuario
+"""
