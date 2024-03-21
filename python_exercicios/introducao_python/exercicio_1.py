34db107df38c8fe7df7ee12b0a1d1bdebf1c7777
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-20 21:43:56 -0300

    exercicio 1

---
+++ python_exercicios/introducao_python/exercicio_1.py
@@ -0,0 +1,24 @@
+""" 
+Exercício de fixação 1: Elabore um algoritmo que peça ao usuário um número 
+e informe o quadrado dele (isto é, o número multiplicado por ele mesmo).
+"""
+
+numero = int( input("Digite um número:"))
+
+resultado = numero * 2
+
+print(f"O quadrado do numero {numero} é {resultado}!" )
+
+
+""" 
+PSEUDOCÓDIGO
+
+Entrada: Um numero inteiro
+Saída: o quadrado de um número
+
+Passos:
+- Peça ao usuario para fornecer um numero
+- Guarde o que o usuario digitou em uma variavel  chamada "numero"
+- quadrado = numero * numero
+- Exiba a variavel "quadrado" para o usuario
+"""
