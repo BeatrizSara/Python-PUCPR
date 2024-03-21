61afeb678cb631f2af5b21c2c26871f50887ca33
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-21 15:55:19 -0300

    exercicio 03

--- python_exercicios/introducao_python/exercicio_3.py
+++ python_exercicios/introducao_python/exercicio_3.py
@@ -6,7 +6,22 @@ calcule sua idade com relação ao ano de 2023, sendo que o usuário já fez ani
 # Solicita o ano de nascimento do usuario
 ano_nascimento = int(input("Informe seu ano de nascimento: "))
 
+# Calcula sua idade com relação ao ano de 2023
 ano_atual = 2023
 idade = ano_atual - ano_nascimento
 
+# Informa ao usuário sua idade atual
 print(f"A idade atual é {idade}")
+
+""" 
+Entrada: Ano de nascimento
+
+Saída: idade ate o ano 2023
+
+Passos: 
+    - Peça ao usuario que informe o ano de nascimento
+    - Guarde a resposta do usuario em uma variavel "ano_nascimento"
+    - idade = ano_atual - ano_nascimento
+    - Exiba a variavel "idade" para o usuario
+
+"""
