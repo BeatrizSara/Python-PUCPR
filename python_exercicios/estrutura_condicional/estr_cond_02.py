309bb23d7a148439f9f2a01a56b83454ba3fda9a
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-21 16:36:55 -0300

    exemplo 02

---
+++ python_exercicios/estrutura_condicional/estr_cond_02.py
@@ -0,0 +1,17 @@
+""" 
+Exemplo de aplicação 2: Elabore um programa que solicite ao usuário as notas de determinada disciplina escolar e calcule a média. 
+Se a média for maior ou igual a 7, deve mostrar que o estudante foi aprovado. Vamos pensar em um exemplo direto em Python? 
+Sugiro que você tente implementar por si só, e somente depois consultar a resposta aqui embaixo.
+"""
+disciplina = str(input("Qual dsiciplina deseja verificar a media: "))
+nota_1 = float(input("Informe sua nota um: "))
+nota_2 = float(input("Informe sua nota dois: "))
+nota_3 = float(input("Informe sua nota três: "))
+nota_4 = float(input("Informe sua nota quatro: "))
+
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+if media >= 7.0:
+    print("Estudante APROVADO")
+
+print("Sua media em {} foi {:.1f}".format(disciplina, media))
