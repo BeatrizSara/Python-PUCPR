0d2ad6ed1b063e731208a43e2582f504462c58d4
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-21 23:21:27 -0300

    estrutura condicional 04

---
+++ python_exercicios/estrutura_condicional/estr_cond_04.py
@@ -0,0 +1,5 @@
+""" 
+Exemplo de aplicação 4: Elabore um programa que solicite ao usuário as notas de determinada disciplina escolar e calcule a média. 
+Se a média for maior ou igual a 7, deve mostrar que o estudante foi aprovado. Caso contrário (isto é, se a nota for menor do que 7), 
+mostre uma mensagem ao estudante informando-o que foi reprovado.
+"""
