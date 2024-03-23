60273b55d21e58569063a5427c550ca41bb980aa
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-23 19:51:42 -0300

    exercicio simples 1

---
+++ python_exercicios/estrutura_condicional/exercicio_1.py
@@ -0,0 +1,5 @@
+""" 
+Seleção simples
+Exercício de fixação 1: Crie um programa que pergunte a idade do usuário. Caso seja maior de idade, 
+deve mostrar uma mensagem informando que pode se inscrever para fazer o teste para tirar a carteira de motorista.
+"""
