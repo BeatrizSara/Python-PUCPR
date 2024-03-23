5cb3b3adac1bd15bc805314327eceed6bd1b0dac
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-23 19:49:29 -0300

    exemplo 03

--- python_exercicios/estrutura_condicional/estr_cond_03.py
+++ python_exercicios/estrutura_condicional/estr_cond_03.py
@@ -1,4 +1,5 @@
 """ 
+Seleção Simples
 Exemplo de aplicação 3: Elabore um programa que solicite ao usuário seu ano de nascimento e calcule sua idade. 
 Para ser mais assertivo, também deve perguntar se o usuário já fez aniversário neste ano e analisar a influência dessa informação no cálculo da idade.
 """
