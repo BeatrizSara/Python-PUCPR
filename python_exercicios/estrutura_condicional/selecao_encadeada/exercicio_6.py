853ff73eb9993dd235a1e05da6d4da4f10e4ad73
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-28 23:36:51 -0300

    alterado condicao a selecao simples exercicio 6

--- python_exercicios/estrutura_condicional/selecao_encadeada/exercicio_6.py
+++ python_exercicios/estrutura_condicional/selecao_encadeada/exercicio_6.py
@@ -7,25 +7,23 @@ num_1 = int(input("Digite o primeiro número: "))
 num_2 = int(input("Digite o segundo número: "))
 operacao = str(input("Qual operação deseja executar entre eles: (+), (-), (*) ou (/): "))
 resultado = 0
+invalido = False
 
 if operacao == "+":
     resultado = num_1 + num_2
-    print("O resultado da operação de {} + {} = {}".format(num_1, num_2, resultado))
     
 elif operacao == "-":
     resultado = num_1 - num_2
-    print("O resultado da operação de {} + {} = {}".format(num_1, num_2, resultado))
-
 
 elif operacao == "*":
     resultado = num_1 * num_2
-    print("O resultado da operação de {} + {} = {}".format(num_1, num_2, resultado))
-
 
 elif operacao == "/":
     resultado = num_1 / num_2
-    print("O resultado da operação de {} + {} = {}".format(num_1, num_2, resultado))
-
 else:
+    invalido = True
+
+if invalido:
     print("Valor Inválido")
-    print("Verifique as opções válidas")
+else:
+    print(num_1, operacao, num_2,"=", resultado)
