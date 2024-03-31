120e46d899e0f3bc4da2729b8d7bf39b73a3c87c
Beatriz Sara <bea.sara.santos@outlook.com>
2024-03-31 19:16:19 -0300

    exemplo de aplicacao 2

--- python_exercicios/estrutura_repeticao/exemplo_laco_for_ 2.py
+++ python_exercicios/estrutura_repeticao/exemplo_laco_for_ 2.py
@@ -2,7 +2,6 @@
 Exemplo de aplicação 2: Elabore um programa que solicite três números, 
 some-os e exiba o resultado para o usuário.
 """
-
 # Exemplo de laço for(finito)
 soma = 0
 for i in range(3):
