90dddc0266c2c55b117f752c1636f0fe3db24357
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-01 19:53:07 -0300

    exemplo controle de fluxo 1

---
+++ python_exercicios/estrutura_repeticao/exemplo_controle_de_fluxo_1.py
@@ -0,0 +1,12 @@
+""" 
+Exemplo de aplicação 4: Elabore um programa que solicite ao usuário dez números e efetue a soma, exibindo o resultado. 
+Contudo, se em algum momento o número digitado for 0, deve interromper o laço, mostrando a soma apenas dos valores informados até então.
+"""
+
+soma  = 0
+for i in range(10):
+    numero = int(input("Informe o "+ str(i + 1)+ " numero: "))
+    if numero == 0:
+        break
+    soma += numero
+print("Soma dos números: {} ".format(soma))
