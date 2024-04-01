83bd3ab1778a3abea407225d2dd65a5315cc22a9
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-01 20:00:51 -0300

    exemplo controle de fluxo 1 break

--- python_exercicios/estrutura_repeticao/exemplo_controle_de_fluxo_1.py
+++ python_exercicios/estrutura_repeticao/exemplo_controle_de_fluxo_1.py
@@ -1,4 +1,5 @@
 """ 
+Comando BREAK
 Exemplo de aplicação 4: Elabore um programa que solicite ao usuário dez números e efetue a soma, exibindo o resultado. 
 Contudo, se em algum momento o número digitado for 0, deve interromper o laço, mostrando a soma apenas dos valores informados até então.
 """
