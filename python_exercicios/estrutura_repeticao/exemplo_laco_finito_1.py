e37336686c6b0064c5d6147826640a8da9d4b4be
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-01 19:35:10 -0300

    exemplo 1- laco finito

---
+++ python_exercicios/estrutura_repeticao/exemplo_laco_finito_1.py
@@ -0,0 +1,8 @@
+""" 
+Exemplo de aplicação 1: Elabore um programa que solicite ao usuário seu nome e 
+mostre cada uma das letras do nome em uma linha diferente do console.
+"""
+nome = str(input("Digite seu nome: "))
+
+for letra in nome:
+    print(letra)
