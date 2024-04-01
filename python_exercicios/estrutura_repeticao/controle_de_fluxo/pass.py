a6c9181f71c932dc755cf0a2fa3feec748b227b8
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-01 19:36:22 -0300

    exemplo controle de fluxo PASS

---
+++ python_exercicios/estrutura_repeticao/controle_de_fluxo/pass.py
@@ -0,0 +1,14 @@
+"""  
+Exemplo comando PASS
+O comando pass não faz nada. É usado quando a sintaxe exige um bloco de código, 
+mas você não quer executar nada dentro desse bloco.
+"""
+for i in range(10):
+    if i % 2 ==  0:
+        pass
+    else:
+        print(i)
+        
+#Esses comandos são úteis para controlar o comportamento de loops em situações específicas, 
+# como interromper um loop prematuramente, 
+# pular iterações ou indicar blocos vazios de código.
