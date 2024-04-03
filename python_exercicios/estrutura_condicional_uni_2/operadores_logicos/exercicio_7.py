3dbe378e0d0ac306dc2f3ac3d462d461db80876f
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-02 21:56:26 -0300

    exemplo 7

--- python_exercicios/estrutura_condicional_uni_2/operadores_logicos/exercicio_7.py
+++ python_exercicios/estrutura_condicional_uni_2/operadores_logicos/exercicio_7.py
@@ -1,16 +1,26 @@
 """ 
-Exercício de fixação 7: Crie um programa que pergunte o tamanho de três lados de um triângulo e informe o tipo de triângulo, a saber:
-
+Exercício de fixação 7: Crie um programa que pergunte o tamanho de três lados de um triângulo 
+e informe o tipo de triângulo, a saber:
 Só será um triângulo quando a soma de dois lados sempre for maior que o terceiro lado.
 Equilátero: triângulo com todos os lados iguais.
 Isósceles: triângulo com dois lados iguais.
 Escaleno: triângulo com todos os lados diferentes.
 """
-soma = 0
-num = -1
-while True:
-    num = int(input("Digite um número para somar (0 finaliza): "))
-    if num == 0:
-        break;
-    soma += num
-print("A soma dos números é ", soma)
+print("===== Informe três lados de um triângulo =====")
+
+lado_1 = int(input("Primeiro lado: "))
+lado_2 = int(input("Segundo lado: "))
+lado_3 = int(input("Terceiro lado: "))
+
+if lado_1 + lado_2 > lado_3 and lado_2 + lado_3 > lado_1 and lado_3 + lado_1 > lado_2:
+    print("Esses valores podem formar um triângulo")
+    if lado_1 == lado_2 == lado_3:
+        print("Esses valores podem formar um triângulo EQUILÁTERO")
+    elif lado_1 == lado_2 or lado_2 == lado_3 or lado_3 == lado_2:
+        print("Esses valores podem formar um triângulo ISÓSCELES")
+        
+    else:
+        print("Esses valores podem formar um triângulo ESCALENO")
+else:
+    print("Esses valores NÃO podem formar um triângulo!")
+    print("Tente novamente")
