a3ffa7ea5c0ec123bbfa19c11ea35465127df582
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-02 21:08:05 -0300

    exemplo 5

---
+++ python_exercicios/estrutura_condicional_uni_2/estr_cond_05.py
@@ -0,0 +1,21 @@
+""" 
+Seleção Encadeada
+Exemplo de aplicação 5: Elabore um programa que solicite três números diferentes ao usuário 
+e informe qual deles é o menor.
+"""
+import time
+
+print("Informe três numeros aleatorios a seguir")
+time.sleep(1)  # Pausa por 1 segundo
+numero_1 = int(input("Primeiro numero: "))
+numero_2 = int(input("Segundo numero: "))
+numero_3 = int(input("Terceiro numero: "))
+    
+if numero_1 < numero_2 and numero_1 < numero_3:
+    print("O numero {} é o menor!".format(numero_1))
+
+elif numero_2 < numero_1 and numero_2 < numero_3:
+    print("O numero {} é o menor!".format(numero_2))
+
+else:
+    print("O numero {} é o menor!".format(numero_3))
