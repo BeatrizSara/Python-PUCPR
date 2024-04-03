f684c23b0a31446f0f9b9c598bc0e12b1efbf8fc
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-02 21:08:40 -0300

    exercicio 3

---
+++ python_exercicios/estrutura_condicional_uni_2/selecao_composta/exercicio_3.py
@@ -0,0 +1,12 @@
+""" 
+Seleção composta
+Exercício de fixação 3: Crie um programa que solicite um número ao usuário e informe se é par ou ímpar. 
+Dica: para saber se um número é par ou ímpar, calcular o resto da divisão desse número por 2 (operador %). 
+Se o resultado for 0, o número será par; se for 1, será ímpar.
+"""
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+    print("O número é PAR")
+else:
+    print("O número é IMPAR")
