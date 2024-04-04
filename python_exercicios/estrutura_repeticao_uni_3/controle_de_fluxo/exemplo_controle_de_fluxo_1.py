6d5f143af8eb179a52552c48c8149a379a1b6951
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-03 23:15:31 -1100

    exemplo controle de fluxo 1

--- python_exercicios/estrutura_repeticao_uni_3/controle_de_fluxo/exemplo_controle_de_fluxo_1.py
+++ python_exercicios/estrutura_repeticao_uni_3/controle_de_fluxo/exemplo_controle_de_fluxo_1.py
@@ -3,7 +3,6 @@ Comando BREAK
 Exemplo de aplicação 4: Elabore um programa que solicite ao usuário dez números e efetue a soma, exibindo o resultado. 
 Contudo, se em algum momento o número digitado for 0, deve interromper o laço, mostrando a soma apenas dos valores informados até então.
 """
-
 soma  = 0
 for i in range(10):
     numero = int(input("Informe o "+ str(i + 1)+ " numero: "))
