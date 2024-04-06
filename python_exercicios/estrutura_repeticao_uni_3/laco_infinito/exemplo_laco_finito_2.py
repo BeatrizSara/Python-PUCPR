89ae155bc9766739050898df47b3edff6571eef6
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-05 19:01:06 -0800

    laco finito exemplo 2

--- python_exercicios/estrutura_repeticao_uni_3/laco_infinito/exemplo_laco_finito_2.py
+++ python_exercicios/estrutura_repeticao_uni_3/laco_infinito/exemplo_laco_finito_2.py
@@ -6,5 +6,5 @@ soma = 0
 for i in range(3):
     numero = int(input("Informe o " + str(i +1) + " número: "))
     soma += numero # soma = soma + numero
-    
+
 print("A soma dos numeros foi {}".format(soma))
