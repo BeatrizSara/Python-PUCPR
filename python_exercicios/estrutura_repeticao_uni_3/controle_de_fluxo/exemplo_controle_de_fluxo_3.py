8b72b1d4774d5140a7565edfd17a18b4f9169cb7
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-06 22:34:35 -0300

    exemplo controle de fluxo 3

--- python_exercicios/estrutura_repeticao_uni_3/controle_de_fluxo/exemplo_controle_de_fluxo_3.py
+++ python_exercicios/estrutura_repeticao_uni_3/controle_de_fluxo/exemplo_controle_de_fluxo_3.py
@@ -2,7 +2,6 @@
 Exemplo de aplicação 7: Elabore um programa que solicite um número inteiro ao usuário, 
 validando e imprimindo esse número.
 """
-
 while True:
     try:
         numero = int(input("Informe um numero:"))
