3d9646b26c168315cd164f4298d28e1ade84ba40
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-06 22:33:51 -0300

    controle de fluxo continue

--- python_exercicios/estrutura_repeticao_uni_3/controle_de_fluxo/continue.py
+++ python_exercicios/estrutura_repeticao_uni_3/controle_de_fluxo/continue.py
@@ -3,7 +3,6 @@ Exemplo Comando CONTINUE
 O comando continue é usado para pular a iteração atual do loop e continuar com a próxima iteração, 
 sem executar o restante do código dentro do loop na iteração atual.
 """
-
 for i in range(10):
     if 1 % 2 == 0:
         continue # pula números pares
