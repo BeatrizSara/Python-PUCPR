0c931216dfc13992d0848bd533177f8db607b4ad
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-07 22:52:59 -0300

    exercicio laco de repeticao for 1

---
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for_1.py
@@ -0,0 +1,5 @@
+""" 
+Laços finitos (for)
+Exercício de fixação 1: Crie um programa que solicite ao usuário dez números, 
+contando quantos são pares e quantos são ímpares e informando ao final essas informações.
+"""
