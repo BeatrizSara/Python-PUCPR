127dec472c21153989c2185f9b8bcd7bf34f34ee
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-07 23:05:12 -0300

    exercicio laco for 3

---
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for3.py
@@ -0,0 +1,5 @@
+""" 
+Exercício de fixação 3: Crie um programa que receba um texto digitado pelo usuário 
+e o imprima apenas com consoantes, removendo as vogais. 
+Observação: desconsiderar letras maiúsculas e acentos.
+"""
