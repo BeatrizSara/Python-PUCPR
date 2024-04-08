94bee5dfa86acb50955f652235d9ee6f71dcc721
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-07 23:04:58 -0300

    exercico laco for 5

---
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for_5.py
@@ -0,0 +1,5 @@
+""" 
+Exercício de fixação 5: Crie um programa que peça dois números ao usuário - o primeiro será o numerador e o segundo, o expoente. 
+A saída do programa deve ser o resultado da operação: numerador elevado ao expoente. 
+Observação: não use a função própria do Python que calcula automaticamente potência.
+"""
