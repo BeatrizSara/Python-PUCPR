3b1290815bacd84770b867f30cf034033e3e9ddd
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-07 23:05:29 -0300

    exercicio laco while 6

---
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_while_6.py
@@ -0,0 +1,7 @@
+""" 
+Laços infinitos (while) 
+
+Exercício de fixação 6: Crie um programa que peça para o usuário inserir um login e uma senha. 
+Caso os valores sejam iguais, informar que os dados são inválidos e pedir novamente as informações. 
+Caso contrário, exibir a mensagem "Bem-vindo ao sistema!".
+"""
