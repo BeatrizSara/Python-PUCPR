6c55c9766f043dde5efa7de6f0dac94b74dac47a
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-09 22:24:40 -0600

    desafio exercicio 9

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_desafio_9.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_desafio_9.py
@@ -1,6 +1,5 @@
 """ 
 Desafio
-
 Exercício de fixação 9: Crie um programa que simule um carrinho de compras, solicitando o nome do produto (não pode ser vazio), seu valor (valor decimal, positivo) 
 e quantidade a ser comprada (valor inteiro, positivo). Ao incluir um produto, deve perguntar se o usuário deseja fechar o pedido ou incluir mais produtos. 
 Todos os dados devem ser validados. Ao final da compra, deve ser informado o valor total do pedido.
