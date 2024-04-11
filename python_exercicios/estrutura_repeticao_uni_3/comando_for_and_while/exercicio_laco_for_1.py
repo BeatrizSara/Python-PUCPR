7b7eca77eecd01b621c92334c503e2a4eb37a219
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-11 18:51:55 -0300

    exercicio 1

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for_1.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for_1.py
@@ -3,3 +3,17 @@ Laços finitos (for)
 Exercício de fixação 1: Crie um programa que solicite ao usuário dez números, 
 contando quantos são pares e quantos são ímpares e informando ao final essas informações.
 """
+par = 0 
+impar = 0
+
+for i in range(10):
+    num = int(input("Informe o " + str(i + 1) +" número: "))
+    
+    if num % 2 == 0:
+        par += 1
+
+    else:
+        impar += 1
+
+print("Possui {} pares".format(par))
+print("Possui {} ímpares".format(impar))
