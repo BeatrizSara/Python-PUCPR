0de44fbc4940ce9a8cdd78075298f76e22fed3e4
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-11 18:50:56 -0300

    exercicio 7

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_while_7.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_while_7.py
@@ -2,3 +2,22 @@
 Exercício de fixação 7: Crie um programa que solicite ao usuário vários números e,
 ao digitar 0, calcule a média dos números informados.
 """
+contador = 0
+total = 0
+
+while True:
+    numero = int(input("Informe um número: "))
+    
+    
+    if numero == 0:
+        break
+    
+    else:
+        total += numero
+        contador += 1
+if contador > 0:
+    media = total / contador
+    print("A media dos números digitados foi: {}".format(media))
+
+else:
+    print("Nenhum número foi digitado")
