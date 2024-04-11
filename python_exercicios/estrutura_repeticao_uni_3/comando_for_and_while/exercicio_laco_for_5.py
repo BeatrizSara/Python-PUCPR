1fa8199d2f1f2579ee2a52025339d1bfba8c941a
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-11 18:51:17 -0300

    exercicio 5

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for_5.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for_5.py
@@ -3,3 +3,11 @@ Exercício de fixação 5: Crie um programa que peça dois números ao usuário 
 A saída do programa deve ser o resultado da operação: numerador elevado ao expoente. 
 Observação: não use a função própria do Python que calcula automaticamente potência.
 """
+numerador = int(input("Informe um número para numerador: "))
+expoente = int(input("Informe um número para expoente: "))
+
+resultado = 1
+for i in range(expoente):
+    resultado *= numerador # realizar o breakpoint para entender o que ocorre nessa etapa.
+
+print("O resultado de {} elevado a {} é igual a: {}".format(numerador, expoente,  resultado))
