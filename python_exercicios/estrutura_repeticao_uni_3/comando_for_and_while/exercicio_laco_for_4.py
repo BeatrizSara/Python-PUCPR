1fb85c0f4509ae473f96e6faab320a7829463329
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-11 18:51:27 -0300

    exercicio 4

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for_4.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_for_4.py
@@ -1,13 +1,17 @@
 """ 
 Exercício de fixação 4: Crie um programa que solicite um número ao usuário e 
 exiba a tabuada dele de 1 a 10, no formato:
-Tabuada do n
-
+Tabuada do n:
 n x 1 = n
-
 n x 2 = 2n
-
 ...
-
 n x 10 = 10n
 """
+
+numero = int(input("Qual tabuada você deseja? "))
+print("Tabuada do {}".format(numero))
+
+for i in range(1, 11):
+    resultado = numero*i
+    
+    print("{} x {} = {}".format(numero, i, resultado))
