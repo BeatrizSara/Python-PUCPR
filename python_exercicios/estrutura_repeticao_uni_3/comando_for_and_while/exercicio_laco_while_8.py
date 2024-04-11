de290c15a0579d17e27103052b17910ce653d2be
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-11 18:50:42 -0300

    exercicio 8

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_while_8.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_while_8.py
@@ -4,3 +4,21 @@ enquanto o valor for menor que um valor informado pelo usuário.
 Observação: série de Fibonacci = 0, 1, 1, 2, 3, 5, 8 ,13, 21, 34, 55, ... 
 é formada por 0, 1 e, partir deste ponto, sempre será a soma dos dois valores anteriores.
 """
+valor = 0
+
+while True:
+    
+    max = int(input("Informe um valor limite: "))
+    num_1 = 0
+    num_2 = 1
+    serie = "0, 1"
+    
+    while valor < max:
+        valor = num_1 + num_2
+        if valor < max:
+            num_1 = num_2
+            num_2 = valor
+        serie += ", " + str(valor)
+        valor = num_1 + num_2
+    
+    print("Serie de Fibonacci: {}".format(serie))
