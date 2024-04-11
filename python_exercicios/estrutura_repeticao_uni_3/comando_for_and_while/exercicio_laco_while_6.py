256c6fb45d744c712428af3d8d05b5e77b370997
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-11 18:51:06 -0300

    exercicio 6

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_while_6.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laco_while_6.py
@@ -5,3 +5,14 @@ Exercício de fixação 6: Crie um programa que peça para o usuário inserir um
 Caso os valores sejam iguais, informar que os dados são inválidos e pedir novamente as informações. 
 Caso contrário, exibir a mensagem "Bem-vindo ao sistema!".
 """
+while True:
+
+    login = input("Login: ")
+    senha = input("Senha: ")
+
+    if login == senha:
+        print("Dados Inválidos. Tente acessar novamente")
+    
+    else:
+        print("Bem-Vindo ao sistema!")
+        break # sair do loop quando as informações forem validas
