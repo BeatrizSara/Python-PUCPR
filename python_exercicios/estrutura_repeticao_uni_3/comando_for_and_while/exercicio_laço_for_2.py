cbecab6c15e4d47637927fdc0d4707aa07bb81a2
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-11 18:51:46 -0300

    exercicio 2

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laço_for_2.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_laço_for_2.py
@@ -2,3 +2,13 @@
 Exercício de fixação 2: Crie um programa que peça ao usuário cinco números 
 e informe qual é o menor e qual é o maior deles.
 """
+nums = []
+for i in range(5):
+    num = int(input("Informe o "+ str(i+1) +" número: "))
+    nums.append(num)
+
+maior = max(nums)
+menor = min(nums)
+
+print("O maior número foi {}".format(maior))
+print("O menor número foi {}".format(menor))
