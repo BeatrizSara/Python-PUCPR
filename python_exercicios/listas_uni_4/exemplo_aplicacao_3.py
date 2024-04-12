485a83f254ff3967c695ff535cb4b5370359d0d3
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-12 03:16:54 -0300

    exemplo aplicacao 3

---
+++ python_exercicios/listas_uni_4/exemplo_aplicacao_3.py
@@ -0,0 +1,16 @@
+""" 
+Exemplo de aplicação 3: Dada a lista de dados nums = [1, 4, 23, 11, 8], 
+corra a lista usando um objeto range e imprima cada elemento em uma linha.
+"""
+nums = [1, 4, 23, 11, 8]
+for i in range(len(nums)): 
+    print(nums[i])
+
+# função len(): ela permite saber o comprimento (tamanho) de uma lista
+
+""" Outra forma de obter o mesmo resultado 
+é utilizar o comando for interagindo com cada elemento da lista, como segue:"""
+
+nums = [1, 4, 23, 11, 8]
+for num in nums:
+    print(num)
