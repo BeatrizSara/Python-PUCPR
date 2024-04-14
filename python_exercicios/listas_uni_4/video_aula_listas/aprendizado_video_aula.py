f347f912c15755316a848bb1079b10c666eaaadb
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-13 23:39:32 -0300

    aprendizado video aula

---
+++ python_exercicios/listas_uni_4/video_aula_listas/aprendizado_video_aula.py
@@ -0,0 +1,17 @@
+""" Continuação do raciocinio do exemplo_02.py 
+
+O QUE foi mostrado durante a video aula
+
+Exemplos de:
+- Como criar listas
+- Como criar uma nova lista
+- Como que faz a iteracao entre diferentes elementos de uma lista
+- Como que apaga um elemento
+- Como ver se um elemento existe 
+
+Para as atividades pode usar primeiramente:
+- append()
+- len()
+- remove() ou pop()
+- Olhar a documentacao do Python.org para tentar implementar algumas outras coisas
+"""
