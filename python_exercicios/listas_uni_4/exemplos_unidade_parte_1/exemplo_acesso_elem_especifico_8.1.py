98dee007d7e9beee2ebc59a9487c000f890cabdc
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-13 23:39:56 -0300

    exemplo de acesso a elementos especificos

---
+++ python_exercicios/listas_uni_4/exemplos_unidade_parte_1/exemplo_acesso_elem_especifico_8.1.py
@@ -0,0 +1,8 @@
+""" 
+Acesso a elementos específicos de uma matriz
+Assim como ocorre com vetores, que possuem a capacidade de acesso a um elemento específico 
+a partir do seu índice, os elementos de uma matriz podem ser acessados a partir do índice de cada dimensão. 
+No caso de uma matriz de dimensão 3x3, o elemento central poderia ser acessado a partir de:
+"""
+m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(m[1][1])      # isto mostrará o número inteiro "5"
