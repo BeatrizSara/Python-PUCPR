2f497e06addc32bd71b042fe4ed35ecda60fd20e
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-19 15:42:58 -0300

    exemplo 3

---
+++ python_exercicios/estrutura_dados_uni_5/exemplos_estr_dados/exemplo_3.py
@@ -0,0 +1,9 @@
+""" 
+Exemplo de aplicação 3: Elabore um programa que exemplifique a remoção de itens de um dicionário 
+usando o método pop().
+"""
+agenda = {"Maria": "(41) 98765-4321", "João": "(11) 12345-6789"}
+
+print(agenda.pop("Maria", "Contato com nome Maria localizado"))
+print(agenda.pop("José", "contato com o nome José não localizado"))
+print(agenda)
