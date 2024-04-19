98862192a6a93a92bf186d07c019a0d17ad9b326
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-19 20:51:49 -0300

    exemplo metodo em listas 3

---
+++ python_exercicios/estrutura_dados_uni_5/exemplo_metodo_listas/exemplo_metod_list_3.py
@@ -0,0 +1,19 @@
+# Exemplo - para cada elemento dentro da lista frutas
+frutas = ["banana", "maçã", "laranja", "melão", "abacaxi", "melancia"]
+
+for elemento in frutas:
+    print("O nome da fruta é: {}".format(elemento))
+
+# Acessar cada alemento pelos indices
+
+print(frutas[0]) # Acessar o primeiro elemento
+print(frutas[1]) # Acessar o segundo elemento
+print(frutas[2]) # Acessar o terceiro elemento
+print(frutas[3]) # Acessar o quarto elemento
+print(frutas[4]) # Acessar o primeiro elemento
+print(frutas[5]) # Acessar o primeiro elemento
+
+# Obs: [-1] - indice que conta de tras pra frente
+print(frutas[-1]) # Acessar  o ultimo elemento
+print(frutas[-2]) # Acessar o penultimo elemento
+print(frutas[-3]) # Acessar o antepenultimo elemento) e assim por diante
