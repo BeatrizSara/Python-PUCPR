32045c2f298704aecdfe9749456b656c738c76ce
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-19 15:47:55 -0300

    exemplo 7

---
+++ python_exercicios/estrutura_dados_uni_5/exemplos_estr_dados/exemplo_7.py
@@ -0,0 +1,8 @@
+""" 
+Exemplo de aplicação 7: Elabore um aplicativo que demonstre a necessidade de finalizar 
+uma tupla de um único elemento com uma vírgula no fim.
+"""
+tupla1 = ("Brasil")
+print(type(tupla1))
+tupla2 = ("Brasil",) # com a virgula após a string, entende-se que seja uma Tupla 
+print(type(tupla2))
