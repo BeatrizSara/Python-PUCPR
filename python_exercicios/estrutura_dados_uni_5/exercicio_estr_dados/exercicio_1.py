2ad174c775500a5014708bfc72138ce45178dd44
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-20 23:19:33 -0300

    exercicio 1

--- python_exercicios/estrutura_dados_uni_5/exercicio_estr_dados/exercicio_1.py
+++ python_exercicios/estrutura_dados_uni_5/exercicio_estr_dados/exercicio_1.py
@@ -1,6 +1,6 @@
 """ 
 Exercício de fixação 1: Crie um programa que efetue
-o cadastro de pessoas com nome, RG e CPF por meio de tuplas, 
+o cadastro de pessoas com nome, RG e CPF por meio de tuplas,
 adicionando-as a uma lista e imprimindo essa lista no fim do programa.
 """
 pessoas = []
