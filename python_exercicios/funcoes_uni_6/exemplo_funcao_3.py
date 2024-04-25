9d953619fbf30f1b0ff4e1ed939b214d6a6ffe5a
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-25 19:34:59 -0300

    exemplo 3

---
+++ python_exercicios/funcoes_uni_6/exemplo_funcao_3.py
@@ -0,0 +1,21 @@
+""" 
+Função com parâmetros e sem retorno:
+
+Agora, toda função precisa ter retorno? Não necessariamente. 
+Teste isto:
+
+"""
+def saudacao_personalizada(nome):
+    print(f"Olá,{nome}!")
+    
+# Exemplo de uso da funcao "saudacao_personalizada"
+saudacao_personalizada("Beatriz")
+
+""" 
+Analisemos esta função:
+
+1. Nome da função: saudacao_personalizada
+2. Parâmetros: uma variável (nome)
+3. Retorno: nenhum (dentro desta função somente mostramos uma mensagem. 
+Como não tem return, nada é retornado para ser reaproveitado fora desta função).
+"""
