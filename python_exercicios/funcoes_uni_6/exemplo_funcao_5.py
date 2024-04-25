95187743737fc1cf070d529be57054fb987b0eaa
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-25 19:35:39 -0300

    exemplo  5

---
+++ python_exercicios/funcoes_uni_6/exemplo_funcao_5.py
@@ -0,0 +1,24 @@
+""" 
+Função sem parâmetros e sem retorno:
+
+E, finalmente, funções que não possuem nem parâmetros e nem retornos. 
+Isso é possível? Claro! Veja só:
+"""
+def saudacao():
+    print("Olá, mundo!")
+
+# Exemplo de uso da função "saudacao"
+saudacao()
+saudacao()
+saudacao()
+
+""" 
+Analisemos esta função:
+
+Nome da função: saudacao
+Parâmetros: nenhum
+Retorno: nenhum (não existe a palavra-chave return para retornar algo).
+
+Neste caso, a função saudacao() simplesmente mostra uma saudação quando chamada. 
+Ela não recebe nenhum parâmetro nem retorna algum valor.
+"""
