2ed9aad220218dac62b8f56a4028718d19355010
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-25 19:35:12 -0300

    exemplo  4

---
+++ python_exercicios/funcoes_uni_6/exemplo_funcao_4.py
@@ -0,0 +1,24 @@
+""" 
+Função sem parâmetros e com retorno:
+
+Também podemos ter funções com retorno, mas sem nenhuma variável. Veja só:
+"""
+
+def get_numero_pi():
+    return 3.141592653589793
+
+# Exemplo de  uso da funcao "get_numero_pi"
+print(f"O valor de pi é {get_numero_pi}")
+print(f"O valor de pi é {get_numero_pi}")
+print(f"O valor de pi é {get_numero_pi}")
+
+""" 
+Analisemos esta função:
+
+Nome da função: get_numero_pi
+Parâmetros: nenhum
+Retorno: um valor (o valor de pi).
+
+Agora, por que esta função não tem parâmetros? Como ela sempre retornará o 
+mesmo resultado, esta função não precisa de nenhuma variável de entrada.
+"""
