a1c8270b5da5b64c3afe0f2565213d71c324fd93
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-25 19:34:52 -0300

    exemplo 2

---
+++ python_exercicios/funcoes_uni_6/exemplo_funcao_2.py
@@ -0,0 +1,21 @@
+""" 
+Agora, sempre precisamos informar aqueles parâmetros e o return? 
+Não. Vejamos alguns exemplos:
+
+Função com parâmetros e com retorno:
+Teste a função abaixo:
+"""
+def soma_tres_numeros(num1, num2, num3):
+    return num1 + num2 + num3
+
+# Exemplo de uso da funcao "soma_tres_numeros"
+print(soma_tres_numeros(5, 10, 2))
+
+""" 
+Analisemos esta função:
+
+1. Nome da função: soma_tres_numeros
+2. Parâmetros: três variáveis (num1, num2 e num3)
+3. Retorno: um valor (o resultado da soma dos três parâmetros).
+
+"""
