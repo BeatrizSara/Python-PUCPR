3318558e6dd9b1f80fb340dd2fb85e90aaca99c6
Beatriz Sara <bea.sara.santos@outlook.com>
2024-04-26 02:42:59 -0300

    funcao pesquisar clientes 1.7

---
+++ python_exercicios/funcoes_uni_6/cadastro_de_clientes_part1/funcao_pesquisar_clientes_.1.7.py
@@ -0,0 +1,20 @@
+""" 
+Função pesquisar_clientes()
+
+Esta é uma função com parâmetros e com retorno. Este código define uma função chamada 
+pesquisar_clientes que recebe dois argumentos: uma lista chamada clientes e uma string chamada nome.
+
+A função pesquisar_clientes procura pelo valor da variável nome dentro da lista clientes. 
+Se encontrar, retorna True, senão retorna False.
+"""
+
+
+
+
+# Vamos analisar linha a linha:
+
+# def pesquisar_clientes(clientes, nome): Isto define a função pesquisar_clientes que aceita dois argumentos: clientes (que é uma lista) e nome (que é uma string).
+# if nome in clientes: Isto verifica se o valor da variável nome existe na lista clientes. O operador inem Python verifica a existência de um item em uma lista.
+# return True: Se o nome está presente na lista clientes, a função retorna True.
+# else: Se o nome não estiver presente na lista clientes, o programa entra nesta cláusula.
+# return False: Se o nome não está presente na lista clientes, a função retorna False.
