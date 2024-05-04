d3fb5812ac051963d831d19d7604415cc2a4bc09
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-04 01:57:17 -0300

    exemplo 4

---
+++ python_exercicios/arquivos_e_excecoes_uni_7/exemplos_aplicacao/exemplo_4.py
@@ -0,0 +1,14 @@
+""" 
+Exemplo de aplicação 4: Leia novamente o conteúdo do arquivo “nome.txt”. 
+Agora, leia uma linha de cada vez, e mostre o conteúdo de cada linha na tela.
+"""
+
+with open("nome.txt", "r") as f:
+    for linha in f.readlines():
+        print(linha)
+
+# Perceba que alguns comportamentos se repetem: o bloco de leitura do arquivo com o with, 
+# e a abertura do arquivo em modo de leitura (“r”). 
+# Agora, veja que o conteúdo do f.readlines() é uma lista de strings. 
+# Cada string é uma linha do arquivo. 
+# Como é uma lista, estamos usando o for para mostrar cada linha individualmente na tela para o usuário.
