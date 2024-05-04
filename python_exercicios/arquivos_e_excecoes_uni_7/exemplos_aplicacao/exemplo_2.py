da91244941bc482255fcba6520c2c1a09eb62de1
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-04 01:56:54 -0300

    exemplo 2

---
+++ python_exercicios/arquivos_e_excecoes_uni_7/exemplos_aplicacao/exemplo_2.py
@@ -0,0 +1,14 @@
+""" Exemplo de aplicação 2: Usando o exemplo de aplicação 1, leia o nome salvo no arquivo “nome.txt”, 
+mostrando cada caractere em uma linha separada."""
+
+with open("nome.txt", "r") as f:
+    conteudo = f.read()
+    
+    for letra in conteudo:
+        print(letra)
+        
+# Veja que, como estamos lendo o arquivo, agora o bloco é with open("nome.txt", "r") as f:. 
+# Aquele "r" indica que estamos abrindo o arquivo "nome.txt" no modo de leitura. 
+# O conteúdo do arquivo é lido e armazenado na variável conteudo.
+
+#Depois, usamos um for para mostrar cada letra dentro de uma única linha.
