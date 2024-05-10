303b1cfcaf9a90cd55c924875027a39f5001b69a
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-09 23:01:19 -0400

    exemplo 2

--- python_exercicios/arquivos_e_excecoes_uni_7/exemplos_aplicacao/exemplo_2.py
+++ python_exercicios/arquivos_e_excecoes_uni_7/exemplos_aplicacao/exemplo_2.py
@@ -10,5 +10,4 @@ with open("nome.txt", "r") as f:
 # Veja que, como estamos lendo o arquivo, agora o bloco é with open("nome.txt", "r") as f:. 
 # Aquele "r" indica que estamos abrindo o arquivo "nome.txt" no modo de leitura. 
 # O conteúdo do arquivo é lido e armazenado na variável conteudo.
-
 #Depois, usamos um for para mostrar cada letra dentro de uma única linha.
