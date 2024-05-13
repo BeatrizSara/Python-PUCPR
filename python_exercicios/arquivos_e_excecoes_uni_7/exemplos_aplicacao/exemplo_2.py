91b199203c0ca3f389837d565dcc16b06ab9ae15
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-12 21:44:08 -0300

    exemplo 2

--- python_exercicios/arquivos_e_excecoes_uni_7/exemplos_aplicacao/exemplo_2.py
+++ python_exercicios/arquivos_e_excecoes_uni_7/exemplos_aplicacao/exemplo_2.py
@@ -1,6 +1,6 @@
 """ Exemplo de aplicação 2: Usando o exemplo de aplicação 1, leia o nome salvo no arquivo “nome.txt”, 
-mostrando cada caractere em uma linha separada."""
-
+mostrando cada caractere em uma linha separada.
+"""
 with open("nome.txt", "r") as f:
     conteudo = f.read()
     
