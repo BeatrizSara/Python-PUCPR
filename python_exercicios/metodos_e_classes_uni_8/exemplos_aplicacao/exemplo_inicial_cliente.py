ba0cf162937b9120170d50d24b6d2d46f51f06cf
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-14 23:15:12 -0300

    exemplo inicial unidade 8

---
+++ python_exercicios/metodos_e_classes_uni_8/exemplos_aplicacao/exemplo_inicial_cliente.py
@@ -0,0 +1,12 @@
+""" 
+Exemplo: a implementação inicial da classe Cliente:
+"""
+
+# Classe Cliente
+ 
+class Cliente:
+    nome = "João"
+    cpf = "123.456.789-00"
+ 
+    def imprimir(self):
+        print(f"Cliente:", self.nome, "- CPF:", self.cpf)
