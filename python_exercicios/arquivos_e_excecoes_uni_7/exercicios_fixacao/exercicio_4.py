153d29f3c3e8a80af1d9e7a34230d487195318f6
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-14 23:14:22 -0300

    exercicio 4

---
+++ python_exercicios/arquivos_e_excecoes_uni_7/exercicios_fixacao/exercicio_4.py
@@ -0,0 +1,14 @@
+""" 
+Exercício de fixação 4: Crie um programa que leia o arquivo "alunos.txt" do exercício anterior. 
+Em seguida, calcule a média das notas dos alunos e exiba na saída padrão. 
+Dica: se quiser, pesquise sobre o método “split” em Python. Ele serve para dividir a string em strings menores.
+"""
+with open("alunos.txt", "r") as arquivo:
+    notas = []
+    for linha in arquivo:
+        dados = linha.split(",")
+        notas.append(float(dados[2]))
+        
+    media = sum(notas) / len(notas)
+
+    print("A média das notas dos alunos é:", media)
