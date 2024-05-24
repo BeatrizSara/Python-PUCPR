a23507ce6aee915e6bf84b5ca9692bdb454aeba5
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-23 23:03:48 -0300

    exemplo 2

--- python_exercicios/metodos_e_classes_uni_8/exemplos_aplicacao/exemplo_2.py
+++ python_exercicios/metodos_e_classes_uni_8/exemplos_aplicacao/exemplo_2.py
@@ -2,7 +2,6 @@
 Exemplo de aplicação 2: Elabore uma classe que simule um dado de jogo. No construtor, deve-se informar quantas faces possui o dado. Caso o valor informado seja inválido, o dado será padrão, de seis faces. Deve ter um método privado de calcular a jogada e um método público de retorno do valor do dado para o usuário. 
 Ao final, deve testar a classe e todos os seus métodos.
 """
-
 import random
  
 class Dado:
