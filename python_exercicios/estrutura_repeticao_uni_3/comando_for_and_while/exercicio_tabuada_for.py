400a67cd4de8fda5880b26084c11afcf59cb7844
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-25 23:48:42 -0300

    editado exercicio tabuada for

--- python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_tabuada_for.py
+++ python_exercicios/estrutura_repeticao_uni_3/comando_for_and_while/exercicio_tabuada_for.py
@@ -1,6 +1,6 @@
 """ Forme um atabuada utilizando o comando for 
-de modo a economizar códigos, sem utilizar muitas linhas"""
-
+de modo a economizar códigos, sem utilizar muitas linhas
+"""
 numero = int(input("Qual tabuada deseja: "))
 
 for i in range(0, 11):
