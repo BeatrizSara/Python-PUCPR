7d3d6444f363429577260be93694a890b379a497
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-28 22:11:58 -0300

    editado

--- python_exercicios/funcoes_uni_6/cadastro_de_clientes_part1/funcao_menu_1.2.py
+++ python_exercicios/funcoes_uni_6/cadastro_de_clientes_part1/funcao_menu_1.2.py
@@ -5,7 +5,6 @@ Esta é uma função sem parâmetros e sem retorno.
 Esta é uma forma simples e eficaz de exibir um menu de opções para o usuário em um programa de console. 
 O usuário pode então inserir a sua escolha (tipicamente através de uma função de input após a chamada da função menu()).
 """
-
 def menu():
     print("\nMenu:")
     print("1. Inserir clientes")
