d9809b123aaea91a075900a3fd79774290972f6f
Beatriz Sara <bea.sara.santos@outlook.com>
2024-05-30 20:19:41 -0300

    exemplo funcao range

--- python_exercicios/estrutura_repeticao_uni_3/exemplo_funcao_range.py
+++ python_exercicios/estrutura_repeticao_uni_3/exemplo_funcao_range.py
@@ -4,7 +4,7 @@ exibindo a informação de como é feito o cálculo.
 Exemplo: “O valor do fatorial de 5 é igual a 120. Expressão: 5*4*3*2*1”. 
 """
 fatorial = 1
-expressao = "Expressão: " # variável será usada para construir a expressão do fatorial.
+expressao = "Expressão: "# variável será usada para construir a expressão do fatorial.
 numero = int(input("Informe um numero para código fatorial: "))
 
 for i in range(numero, 0, -1):
